eea87e738dd3556525054fcbf4e3dcde5270d290
jancarlohendriks <jancarlohendriks@hotmail.com>
2022-10-07 21:04:02 +0200

    minor clean up

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
-// const http = require("http");
-// const https = require("https");
-// const fs = require("fs");
-// const key = fs.readFileSync("./key.pem", "utf8");
-// const cert = fs.readFileSync("./cert.pem", "utf8");
 const path = require("path");
-
 const axios = require("axios");
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const { PORT, AUTH_URL, ACCESS_URL, CLIENT_ID, CLIENT_SECRET } = process.env;
 const REDIRECT_URI = `http://localhost:${PORT}/oauth-callback`;
 
 const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(cors({ credentials: true, origin: true, allowedHeaders: true }));
-
-// const httpServer = http.createServer(app);
-// const server = https.createServer({ key: key, cert: cert }, app);
+app.use(cors({ credentials: true, origin: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/index.html"));
@@ -36,12 +22,8 @@ app.get("/auth", (req, res) => {
   );
 });
 
-app.get("/oauth-callback", async (req, res) => {
+app.get("/oauth-callback", (req, res) => {
   const code = req.query.code;
-  const headers = {
-    Accept: "*/*",
-    "Content-Type": "application/x-www-form-urlencoded",
-  };
   const body = {
     grant_type: "authorization_code",
     redirect_uri: `http://localhost:${PORT}/oauth-callback`,
@@ -49,29 +31,16 @@ app.get("/oauth-callback", async (req, res) => {
     client_secret: CLIENT_SECRET,
     code: code,
   };
-
-  await axios({
+  axios({
     method: "post",
     url: ACCESS_URL,
-    headers: headers,
     data: body,
   })
     .then((response) => {
       console.log(response);
-      res.json(response.data.access_token);
+      res.json(response.data);
     })
     .catch((err) => res.status(500).json(err));
-
-  // axios
-  //   .post(ACCESS_URL, body)
-  //   .then((response) => {
-  //     console.log(response);
-  //     res.json(response);
-  //   })
-  //   .catch((err) => res.status(500).json({ err }));
-
-  // console.log(cookie);
-  // res.send("hello");
 });
 
 app.listen(PORT, () => {
