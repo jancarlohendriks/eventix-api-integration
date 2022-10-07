316b5254fd4a159a8a0e93482c854b313bb9da03
jancarlohendriks <jancarlohendriks@hotmail.com>
2022-10-07 19:55:47 +0200

    minor change

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// const querystring = require("querystring");
 const axios = require("axios");
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-// const fetch = require("node-fetch");
 require("dotenv").config();
 
 const { PORT, AUTH_URL, ACCESS_URL, CLIENT_ID, CLIENT_SECRET } = process.env;
@@ -13,29 +11,33 @@ const REDIRECT_URI = encodeURIComponent(
 
 const app = express();
 app.use(cors({ credentials: true, origin: true, allowedHeaders: true }));
+// app.use(express.static("views"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/index.html"));
 });
 
-app.get("/auth", async (req, res) => {
+app.get("/auth", (req, res) => {
   res.redirect(
     `${AUTH_URL}?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`
   );
 });
 
 app.get("/oauth-callback", async (req, res) => {
-  const code = req.query.code;
-  // const cookie = req;
-  const opts = { headers: { accept: "application/json" } };
-  const body = {
-    grant_type: "authorization_code",
-    client_id: CLIENT_ID,
-    client_secret: CLIENT_SECRET,
-    code: code,
-    // scope: "public",
-    // state: "active"
-  };
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+
+  // const code = req.query.code;
+  // // const cookie = req;
+  // const opts = { headers: { accept: "application/json" } };
+  // const body = {
+  //   grant_type: "authorization_code",
+  //   client_id: CLIENT_ID,
+  //   client_secret: CLIENT_SECRET,
+  //   code: code,
+  //   redirect_uri: REDIRECT_URI,
+  //   // scope: "public",
+  //   // state: "active"
+  // };
 
   // const params = new URLSearchParams();
   // params.append("grant_type", "authorization_code");
@@ -43,27 +45,42 @@ app.get("/oauth-callback", async (req, res) => {
   // params.append("redirect_uri", REDIRECT_URI);
   // params.append("client_id", CLIENT_ID);
   // params.append("client_secret", CLIENT_SECRET);
+  // params.append("redirect_uri", REDIRECT_URI);
 
-  // const api_token = fetch(url, {
+  // // const api_token = fetch(url, {
+  // //   method: "post",
+  // //   headers: {
+  // //     "Content-Type": "application/x-www-form-urlencoded",
+  // //   },
+  // //   body: params,
+  // // }).then((response) => {
+  // //   res.json(response);
+  // // });
+
+  // axios({
   //   method: "post",
+  //   url: `${ACCESS_URL}?grant_type=authorization_code&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${code}`,
   //   headers: {
-  //     "Content-Type": "application/x-www-form-urlencoded",
+  //     Accept: "application/json",
   //   },
-  //   body: params,
-  // }).then((response) => {
-  //   res.json(response);
-  // });
+  //   // headers: opts,
+  // })
+  //   .then((response) => {
+  //     console.log(response);
+  //     res.json(response);
+  //   })
+  //   .catch((err) => res.status(500).json(err));
 
-  axios
-    .post(ACCESS_URL, body, opts)
-    .then((response) => {
-      console.log(response);
-      res.json(response);
-    })
-    .catch((err) => res.status(500).json({ err }));
+  // // axios
+  // //   .post(ACCESS_URL, body, opts)
+  // //   .then((response) => {
+  // //     console.log(response);
+  // //     res.json(response);
+  // //   })
+  // //   .catch((err) => res.status(500).json({ err }));
 
-  // console.log(cookie);
-  // res.send("hello");
+  // // console.log(cookie);
+  // // res.send("hello");
 });
 
 app.listen(PORT, () => {
