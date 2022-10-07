3b46946165accbee1033116b82580fc0ec122380
jancarlohendriks <jancarlohendriks@hotmail.com>
2022-10-07 20:49:25 +0200

    auth token working

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
+// const http = require("http");
+// const https = require("https");
+// const fs = require("fs");
+// const key = fs.readFileSync("./key.pem", "utf8");
+// const cert = fs.readFileSync("./cert.pem", "utf8");
+const path = require("path");
+
 const axios = require("axios");
 const express = require("express");
-const path = require("path");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const { PORT, AUTH_URL, ACCESS_URL, CLIENT_ID, CLIENT_SECRET } = process.env;
-const REDIRECT_URI = encodeURIComponent(
-  `http://localhost:${PORT}/oauth-callback`
-);
+const REDIRECT_URI = `http://localhost:${PORT}/oauth-callback`;
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cors({ credentials: true, origin: true, allowedHeaders: true }));
-// app.use(express.static("views"));
+
+// const httpServer = http.createServer(app);
+// const server = https.createServer({ key: key, cert: cert }, app);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/index.html"));
@@ -19,70 +30,50 @@ app.get("/", (req, res) => {
 
 app.get("/auth", (req, res) => {
   res.redirect(
-    `${AUTH_URL}?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`
+    `${AUTH_URL}?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
+      REDIRECT_URI
+    )}`
   );
 });
 
 app.get("/oauth-callback", async (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
+  const code = req.query.code;
+  const headers = {
+    Accept: "*/*",
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+  const body = {
+    grant_type: "authorization_code",
+    redirect_uri: `http://localhost:${PORT}/oauth-callback`,
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    code: code,
+  };
 
-  // const code = req.query.code;
-  // // const cookie = req;
-  // const opts = { headers: { accept: "application/json" } };
-  // const body = {
-  //   grant_type: "authorization_code",
-  //   client_id: CLIENT_ID,
-  //   client_secret: CLIENT_SECRET,
-  //   code: code,
-  //   redirect_uri: REDIRECT_URI,
-  //   // scope: "public",
-  //   // state: "active"
-  // };
+  await axios({
+    method: "post",
+    url: ACCESS_URL,
+    headers: headers,
+    data: body,
+  })
+    .then((response) => {
+      console.log(response);
+      res.json(response.data.access_token);
+    })
+    .catch((err) => res.status(500).json(err));
 
-  // const params = new URLSearchParams();
-  // params.append("grant_type", "authorization_code");
-  // params.append("code", code);
-  // params.append("redirect_uri", REDIRECT_URI);
-  // params.append("client_id", CLIENT_ID);
-  // params.append("client_secret", CLIENT_SECRET);
-  // params.append("redirect_uri", REDIRECT_URI);
-
-  // // const api_token = fetch(url, {
-  // //   method: "post",
-  // //   headers: {
-  // //     "Content-Type": "application/x-www-form-urlencoded",
-  // //   },
-  // //   body: params,
-  // // }).then((response) => {
-  // //   res.json(response);
-  // // });
-
-  // axios({
-  //   method: "post",
-  //   url: `${ACCESS_URL}?grant_type=authorization_code&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${code}`,
-  //   headers: {
-  //     Accept: "application/json",
-  //   },
-  //   // headers: opts,
-  // })
+  // axios
+  //   .post(ACCESS_URL, body)
   //   .then((response) => {
   //     console.log(response);
   //     res.json(response);
   //   })
-  //   .catch((err) => res.status(500).json(err));
+  //   .catch((err) => res.status(500).json({ err }));
 
-  // // axios
-  // //   .post(ACCESS_URL, body, opts)
-  // //   .then((response) => {
-  // //     console.log(response);
-  // //     res.json(response);
-  // //   })
-  // //   .catch((err) => res.status(500).json({ err }));
-
-  // // console.log(cookie);
-  // // res.send("hello");
+  // console.log(cookie);
+  // res.send("hello");
 });
 
 app.listen(PORT, () => {
-  console.log(`app listening on http://localhost:${PORT}`);
+  console.log(`app listening on https://localhost:${PORT}`);
 });
